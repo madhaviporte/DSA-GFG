045e705cda378de436eeb8187d04a2fb9c4fe06b
Madhavi Porte <157514020+madhaviporte@users.noreply.github.com>
2026-08-06 18:21:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Increment or Double Operations to Convert/minimum-increment-or-double-operations-to-convert.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countMinOperations(int arr[]) {
+        // code here
+        int incs = 0, maxi = 0;
+        
+        for (int a: arr) {
+            incs += Integer.bitCount(a);
+            maxi = Math.max(maxi, a);
+        }
+        
+        return incs + 31 - Integer.numberOfLeadingZeros(maxi);
+    }
+}
