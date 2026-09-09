0dd1b5d02d3afc172f83dd091b854992989ff94d
Madhavi Porte <157514020+madhaviporte@users.noreply.github.com>
2026-09-10 01:34:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.java
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -3,19 +3,17 @@ class Solution {
         // code here
         int n = arr.length;
         int max = -1;
-        
         for(int i=0; i<n; i++){
-            if(max<arr[i]){
+            if(arr[i]>max){
                 max = arr[i];
             }
         }
-        
-        int sMax = -1;
+        int Smax = -1;
         for(int i=0; i<n; i++){
-            if(arr[i]>sMax && arr[i]!=max){
-                sMax = arr[i];
+            if(arr[i]>Smax && arr[i]!=max){
+                Smax = arr[i];
             }
         }
-        return sMax;
+        return Smax;
     }
 }
