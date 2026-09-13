a7822af28784701260d79d68060539664d1f0300
Madhavi Porte <157514020+madhaviporte@users.noreply.github.com>
2026-09-13 13:26:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.java
+++ Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.java
@@ -1,43 +1,13 @@
 class Solution {
     public static ArrayList<Integer> findUnion(int a[], int b[]) {
         // code here
-          ArrayList<Integer> ans = new ArrayList<>();
-        int n = a.length;
-        int m = b.length;
-        int i=0;
-        int j=0;
-        
-        while(i<n && j<m){
-            
-            if(a[i]<b[j]){
-                if(ans.isEmpty() || ans.get(ans.size() -1)!= a[i])
-                ans.add(a[i]);
-                i++;
-            }
-            else if(a[i]>b[j]){
-                if(ans.isEmpty() || ans.get(ans.size()-1)!=b[j])
-                ans.add(b[j]);
-                j++;
-            }
-            else{
-                if(ans.isEmpty() || ans.get(ans.size()-1)!=a[i])
-                ans.add(a[i]);
-                i++;
-                j++;
-            }
+        TreeSet<Integer> set = new TreeSet<>();
+        for(int num : a){
+            set.add(num);
         }
-        while (i < a.length) {
-                    if (ans.isEmpty() || ans.get(ans.size() - 1) != a[i])
-                        ans.add(a[i]);
-                    i++;
-                }
-
-                while (j < b.length) {
-                    if (ans.isEmpty() || ans.get(ans.size() - 1) != b[j])
-                        ans.add(b[j]);
-                    j++;
-                }
-
-                return ans;
+        for(int num : b){
+            set.add(num);
+        }
+        return new ArrayList<Integer>(set);
     }
 }
